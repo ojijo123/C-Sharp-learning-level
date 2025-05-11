cf588e03d937249892ba1cfa042f28ff4b2dbeed
root <ojijo123>
2025-05-11 03:01:54 -0700

    multiple variables

---
+++ c-sharp_variables/variable_4.cs
@@ -0,0 +1,14 @@
+using System;
+namespace MyApplicaiction
+{
+	using program;
+	{
+		static void Main (string [] args)
+		{
+			int y, x, z;
+			y = x = z = 50;
+			Console.WriteLine(y + x + z);
+		}
+	}
+}
+
