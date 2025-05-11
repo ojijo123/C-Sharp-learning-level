bf66dc95242d11307382e504cbf5c88007487110
root <ojijo123>
2025-05-11 01:56:38 -0700

    variable number

---
+++ c-sharp_variables/README.md
@@ -0,0 +1,1 @@
+c-sharp variable project
---
+++ c-sharp_variables/variable_1.cs
@@ -0,0 +1,13 @@
+using System;
+namespace MyApplication
+
+{
+	class program
+	{
+		static void Main (string [] args)
+		{
+			int myNum = 5;
+		Console.WriteLine(myNum)
+	}
+	}
+}
